18b7cc3f0e51c2605863535e329ef5b2ea875efc
hamzahzr <31441239+hamzahzr@users.noreply.github.com>
2026-09-07 22:23:49 +0700

    Log user logout activity

--- api/logout.php
+++ api/logout.php
@@ -1,5 +1,9 @@
 <?php
 require __DIR__ . '/lib.php';
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') json_response(['error'=>'Method not allowed'],405);
+$user=current_user();
+if($user) log_activity('Logout',$user,'Logout berhasil');
 $_SESSION=[];
 if(ini_get('session.use_cookies')){ $p=session_get_cookie_params(); setcookie(session_name(),'',['expires'=>time()-42000,'path'=>$p['path'],'secure'=>$p['secure'],'httponly'=>$p['httponly'],'samesite'=>$p['samesite']??'Lax']); }
-session_destroy(); json_response(['ok'=>true]);
+session_destroy();
+json_response(['ok'=>true]);
